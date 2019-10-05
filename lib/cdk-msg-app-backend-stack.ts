b23d8080a58f9940560547f51203f9d4b11d50fb
Uriel Ramirez <aurbac@gmail.com>
2019-10-05 00:49:17 -0500

    CDK CodePipeline

--- lib/cdk-msg-app-backend-stack.ts
+++ lib/cdk-msg-app-backend-stack.ts
@@ -1,12 +1,21 @@
 import cdk = require('@aws-cdk/core');
-
 import dynamodb = require("@aws-cdk/aws-dynamodb");
+
 import ec2 = require("@aws-cdk/aws-ec2");
+
 import ecr = require("@aws-cdk/aws-ecr");
+
 import ecs = require("@aws-cdk/aws-ecs");
 import iam = require("@aws-cdk/aws-iam");
+
 import ecs_patterns = require("@aws-cdk/aws-ecs-patterns");
 import elbv2 = require('@aws-cdk/aws-elasticloadbalancingv2');
+
+import codecommit = require('@aws-cdk/aws-codecommit');
+
+import codebuild = require('@aws-cdk/aws-codebuild');
+import codepipeline = require('@aws-cdk/aws-codepipeline');
+import codepipeline_actions = require('@aws-cdk/aws-codepipeline-actions');
 
 export class CdkMsgAppBackendStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -38,7 +47,8 @@ export class CdkMsgAppBackendStack extends cdk.Stack {
     
     const repository = new ecr.Repository(this, "Api", {
       repositoryName: "my-api"
-    });
+    });    
+    
     
     const cluster = new ecs.Cluster(this, "MyCluster", {
       vpc: vpc
@@ -56,7 +66,7 @@ export class CdkMsgAppBackendStack extends cdk.Stack {
                 "logs:PutLogEvents"
             ]
     });
-    
+
     const fargateTaskDefinition = new ecs.FargateTaskDefinition(this, 'ApiTaskDefinition', {
       memoryLimitMiB: 512,
       cpu: 256,
@@ -67,8 +77,8 @@ export class CdkMsgAppBackendStack extends cdk.Stack {
       resources: ['*'],
       actions: ['dynamodb:*']
     }));
-    
-    const container = fargateTaskDefinition.addContainer("Api", {
+
+    const container = fargateTaskDefinition.addContainer("backend", {
       // Use an image from Amazon ECR
       image: ecs.ContainerImage.fromRegistry(repository.repositoryUri),
       environment: { 
@@ -77,14 +87,14 @@ export class CdkMsgAppBackendStack extends cdk.Stack {
       }
       // ... other options here ...
     });
-    
+
     container.addPortMappings({
       containerPort: 3000
     });
     
     const sg_service = new ec2.SecurityGroup(this, 'MySGService', { vpc: vpc });
     sg_service.addIngressRule(ec2.Peer.ipv4('0.0.0.0/0'), ec2.Port.tcp(3000));
-    
+
     const service = new ecs.FargateService(this, 'Service', {
       cluster,
       taskDefinition: fargateTaskDefinition,
@@ -92,7 +102,7 @@ export class CdkMsgAppBackendStack extends cdk.Stack {
       assignPublicIp: false,
       securityGroup: sg_service
     });
-    
+
     // Setup AutoScaling policy
     const scaling = service.autoScaleTaskCount({ maxCapacity: 6, minCapacity: 2 });
     scaling.scaleOnCpuUtilization('CpuScaling', {
@@ -117,6 +127,76 @@ export class CdkMsgAppBackendStack extends cdk.Stack {
     });
 
     listener.connections.allowDefaultPortFromAnyIpv4('Open to the world');
+    
+    const code = new codecommit.Repository(this, 'Repository' ,{
+      repositoryName: 'msg-app-backend',
+      description: 'Node.js backend.', // optional property
+    });
+    
+    const project = new codebuild.PipelineProject(this, 'MyProject',{
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_2_0,
+        privileged: true
+      },
+    });
+    const buildRolePolicy =  new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      resources: ['*'],
+      actions: [
+                "ecr:GetAuthorizationToken",
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:GetRepositoryPolicy",
+                "ecr:DescribeRepositories",
+                "ecr:ListImages",
+                "ecr:DescribeImages",
+                "ecr:BatchGetImage",
+                "ecr:InitiateLayerUpload",
+                "ecr:UploadLayerPart",
+                "ecr:CompleteLayerUpload",
+                "ecr:PutImage"
+            ]
+    });
+    project.addToRolePolicy(buildRolePolicy);
+    
+    const sourceOutput = new codepipeline.Artifact();
+    const buildOutput = new codepipeline.Artifact();
+    const sourceAction = new codepipeline_actions.CodeCommitSourceAction({
+      actionName: 'CodeCommit',
+      repository: code,
+      output: sourceOutput,
+    });
+    const buildAction = new codepipeline_actions.CodeBuildAction({
+      actionName: 'CodeBuild',
+      project,
+      input: sourceOutput,
+      outputs: [buildOutput],
+    });
+    
+    new codepipeline.Pipeline(this, 'MyPipeline', {
+      stages: [
+        {
+          stageName: 'Source',
+          actions: [sourceAction],
+        },
+        {
+          stageName: 'Build',
+          actions: [buildAction],
+        },
+        {
+          stageName: 'Deploy',
+          actions: [
+            new codepipeline_actions.EcsDeployAction({
+              actionName: "ECS-Service",
+              service: service, 
+              input: buildOutput
+            }
+            )
+          ]
+        }
+      ],
+    });
+    
     
   }
 }
